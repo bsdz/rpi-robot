b09c6e2a1a16bc13aa8a77abb05f4b95e59671ec
Blair Azzopardi <blairuk@gmail.com>
2017-10-05 05:49:22 +0000

    add speed sensor. minor fixes

---
+++ robot/hardware/speed_sensor.py
@@ -0,0 +1,34 @@
+import time
+
+from robot.hardware.gpio import pi, INPUT, EITHER_EDGE
+from robot.utility.logger import Logger
+log = Logger("Main").get_log()
+
+def cbf(gpio, level, tick):
+   print(gpio, level, tick)
+
+class SpeedSensor(object):
+    log = Logger("SpeedSensor").get_log()
+
+    def __init__(self, name, gpio1, gpio2):
+        self.name = name
+        self.gpio1 = gpio1
+        self.gpio2 = gpio2
+        self.pigpio = pi()
+
+        self.pigpio.set_mode(gpio1, INPUT)
+        self.pigpio.set_mode(gpio2, INPUT)
+
+        self.pigpio.callback(gpio1, EITHER_EDGE, cbf)
+        self.pigpio.callback(gpio2, EITHER_EDGE, cbf)
+        
+def main():
+
+    s = SpeedSensor("motor speed sensors", 19, 26)
+
+    import time
+    while True:
+        time.sleep(.1)
+
+if __name__ == "__main__":
+    main()
