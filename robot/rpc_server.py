7de6215a6b9388dbdf14b5f8c8ab29deac3cb898
Blair Azzopardi <blairuk@gmail.com>
2017-10-29 10:22:02 +0000

    update rpc server

--- robot/rpc_server.py
+++ robot/rpc_server.py
@@ -4,32 +4,32 @@ import Pyro4.core
 import Pyro4.naming
 import Pyro4.socketutil
 
-#import Pyro4
-
 import robot.settings as settings
 from robot.hardware.system import SystemInfo
 
-Pyro4.config.SERVERTYPE = "multiplex"
-Pyro4.config.POLLTIMEOUT = 3
 
-nameserverUri, nameserverDaemon, broadcastServer = Pyro4.naming.startNS(host=settings.robot_ip_address)
+def main():
+    Pyro4.config.SERVERTYPE = "multiplex"
+    Pyro4.config.POLLTIMEOUT = 3
 
-daemon = Pyro4.Daemon(host=settings.robot_ip_address)
+    nsUri, nsDaemon, bcServer = Pyro4.naming.startNS(host=settings.robot_ip_address, port=9090, enableBroadcast=False)
+    daemon = Pyro4.Daemon(host=settings.robot_ip_address)
+
+    ExposedSystemInfo = Pyro4.expose(SystemInfo)
+    uri_SystemInfo = daemon.register(ExposedSystemInfo)
+    nsDaemon.nameserver.register("robot.hardware.system.SystemInfo", uri_SystemInfo)
+
+    daemon.combine(nsDaemon)
+
+    def loopcondition():
+        print(time.asctime(), "Waiting for requests...")
+        return True
+
+    daemon.requestLoop(loopcondition)
+
+    nsDaemon.close()
+    daemon.close()
 
 
-ExposedSystemInfo = Pyro4.expose(SystemInfo)
-uri_SystemInfo = daemon.register(ExposedSystemInfo)
-nameserverDaemon.nameserver.register("robot.hardware.system.SystemInfo", uri_SystemInfo)
-
-daemon.combine(nameserverDaemon)
-daemon.combine(broadcastServer)
-
-def loopcondition():
-    print(time.asctime(), "Waiting for requests...")
-    return True
-
-daemon.requestLoop(loopcondition)
-
-nameserverDaemon.close()
-broadcastServer.close()
-daemon.close()
+if __name__ == "__main__":
+    main()
