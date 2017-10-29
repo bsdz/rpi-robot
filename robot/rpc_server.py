68d8e4ec16fc411940015009f2a7db871bbd471d
bsdz <bsdz@users.noreply.github.com>
2017-10-29 09:50:22 +0000

    Combine pyro nameserver into main daemon

--- robot/rpc_server.py
+++ robot/rpc_server.py
@@ -1,11 +1,35 @@
-from robot.hardware.system import SystemInfo
-import Pyro4
+import time
 
-daemon = Pyro4.Daemon()
-ns = Pyro4.locateNS()
+import Pyro4.core
+import Pyro4.naming
+import Pyro4.socketutil
+
+#import Pyro4
+
+import robot.settings as settings
+from robot.hardware.system import SystemInfo
+
+Pyro4.config.SERVERTYPE = "multiplex"
+Pyro4.config.POLLTIMEOUT = 3
+
+nameserverUri, nameserverDaemon, broadcastServer = Pyro4.naming.startNS(host=settings.robot_ip_address)
+
+daemon = Pyro4.Daemon(host=settings.robot_ip_address)
+
 
 ExposedSystemInfo = Pyro4.expose(SystemInfo)
 uri_SystemInfo = daemon.register(ExposedSystemInfo)
-ns.register("robot.hardware.system.SystemInfo", uri_SystemInfo)
+nameserverDaemon.nameserver.register("robot.hardware.system.SystemInfo", uri_SystemInfo)
 
-daemon.requestLoop()
+daemon.combine(nameserverDaemon)
+daemon.combine(broadcastServer)
+
+def loopcondition():
+    print(time.asctime(), "Waiting for requests...")
+    return True
+
+daemon.requestLoop(loopcondition)
+
+nameserverDaemon.close()
+broadcastServer.close()
+daemon.close()
