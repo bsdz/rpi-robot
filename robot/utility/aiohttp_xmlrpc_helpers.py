462e2b7ede55cf51391d0c13598394e84b00ef58
Blair Azzopardi <blairuk@gmail.com>
2017-12-31 17:53:07 +0000

    update aiohttp_xmlrpc helpers

--- robot/utility/aiohttp_xmlrpc_helpers.py
+++ robot/utility/aiohttp_xmlrpc_helpers.py
@@ -5,7 +5,7 @@ Copyright (C) 2017  Blair Azzopardi
 Distributed under the terms of the GNU General Public License (GPL v3)
 '''
 import io
-import json
+import base64
 
 import numpy
 from lxml import etree
@@ -17,7 +17,14 @@ def _(value):
     memfile = io.BytesIO()
     numpy.save(memfile, value)
     memfile.seek(0)
-    el.text = json.dumps(memfile.read().decode('latin-1'))
+    el.text = base64.b64encode(memfile.read()) 
     return el
 
+def numpy_ndarray(x):
+    memfile = io.BytesIO()
+    memfile.write(base64.b64decode(x.text))
+    memfile.seek(0)
+    return numpy.load(memfile)
+
+XML2PY_TYPES["numpy.ndarray"] = numpy_ndarray
 
